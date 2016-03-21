e911dd06a1f2ab3c2dc1bd5be616f715452a57cf
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-03-21 16:20:49 +0800

    Add exclude app

--- default_event_handler.js
+++ default_event_handler.js
@@ -4,6 +4,10 @@ module.exports.handler = function(event, server) {
     /*
      * Event handler here
      * */
+    // if exclude
+    if (SERVERS.exclude.indexOf(event.process.name) !== -1) {
+        return;
+    }
     if (event.event == 'exit') {
         request.post("your url", {           
             form: {                                                            
